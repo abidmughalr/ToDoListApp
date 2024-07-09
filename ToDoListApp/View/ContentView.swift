7411e51baa8846f16c8796e8efc3bb25c603ac87
abidmughalr <abidmughalr@gmail.com>
2024-07-09 16:16:25 -0500

    small change to Preview

--- ToDoListApp/View/ContentView.swift
+++ ToDoListApp/View/ContentView.swift
@@ -47,8 +47,6 @@ struct MainContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        MainContentView()
-    }
+#Preview {
+    MainContentView()
 }
